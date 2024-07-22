db67cd5f5c14db50b7e9619c6da69540af356e86
shosei <54176240+sho-chan-081@users.noreply.github.com>
2024-07-22 18:44:43 +0900

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,25 +7,22 @@ if wezterm.config_builder then
 end
 
 -- カラースキーマ
-config.color_scheme = 'Gruvbox Dark'
+config.color_scheme = 'iceberg-dark'
 
 config.colors = {
     foreground = 'silver'
 }
 
 -- 背景透過
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.90
 
 -- フォント
 config.font = wezterm.font("JetBrains Mono", {weight="Regular", stretch="Normal", italic=false})
-config.font_size = 16
+config.font_size = 14
 
--- タブを下に表示
-config.tab_bar_at_bottom = true
-
--- マウス操作の挙動
+-- mouse_settings
 config.mouse_bindings = {
-    -- 右クリックでクリップボードから貼り付け
+    -- copy 
     {
         event = { Down = { streak = 2, button = 'Right' } },
         mods = 'NONE',
@@ -33,7 +30,113 @@ config.mouse_bindings = {
     },
 }
 
--- キーバインド
+-- key_bindings
 config.keys = {}
+
+-- not use ime
+config.use_ime = true
+
+-- auto reload
+config.automatically_reload_config = false
+
+-- window_settings
+config.tab_bar_at_bottom = true
+
+wezterm.on('update-right-status', function(window, pane)
+    -- Each element holds the text for a cell in a "powerline" style << fade
+    local cells = {}
+  
+    -- Figure out the cwd and host of the current pane.
+    -- This will pick up the hostname for the remote host if your
+    -- shell is using OSC 7 on the remote host.
+    local cwd_uri = pane:get_current_working_dir()
+    if cwd_uri then
+      local cwd = ''
+      local hostname = ''
+  
+      if type(cwd_uri) == 'userdata' then
+        -- Running on a newer version of wezterm and we have
+        -- a URL object here, making this simple!
+  
+        cwd = cwd_uri.file_path
+        hostname = cwd_uri.host or wezterm.hostname()
+      else
+        -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+        -- which doesn't have the Url object
+        cwd_uri = cwd_uri:sub(8)
+        local slash = cwd_uri:find '/'
+        if slash then
+          hostname = cwd_uri:sub(1, slash - 1)
+          -- and extract the cwd from the uri, decoding %-encoding
+          cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
+            return string.char(tonumber(hex, 16))
+          end)
+        end
+      end
+  
+      -- Remove the domain name portion of the hostname
+      local dot = hostname:find '[.]'
+      if dot then
+        hostname = hostname:sub(1, dot - 1)
+      end
+      if hostname == '' then
+        hostname = wezterm.hostname()
+      end
+  
+      table.insert(cells, cwd)
+      table.insert(cells, hostname)
+    end
+  
+    -- I like my date/time in this style: "Wed Mar 3 08:14"
+    local date = wezterm.strftime '%a %b %-d %H:%M'
+    table.insert(cells, date)
+  
+    -- An entry for each battery (typically 0 or 1 battery)
+    for _, b in ipairs(wezterm.battery_info()) do
+      table.insert(cells, string.format('%.0f%%', b.state_of_charge * 100))
+    end
+  
+    -- The powerline < symbol
+    local LEFT_ARROW = utf8.char(0xe0b3)
+    -- The filled in variant of the < symbol
+    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  
+    -- Color palette for the backgrounds of each cell
+    local colors = {
+      '#3c1361',
+      '#52307c',
+      '#663a82',
+      '#7c5295',
+      '#b491c8',
+    }
+  
+    -- Foreground color for the text across the fade
+    local text_fg = '#c0c0c0'
+  
+    -- The elements to be formatted
+    local elements = {}
+    -- How many cells have been formatted
+    local num_cells = 0
+  
+    -- Translate a cell into elements
+    function push(text, is_last)
+      local cell_no = num_cells + 1
+      table.insert(elements, { Foreground = { Color = text_fg } })
+      table.insert(elements, { Background = { Color = colors[cell_no] } })
+      table.insert(elements, { Text = ' ' .. text .. ' ' })
+      if not is_last then
+        table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+        table.insert(elements, { Text = SOLID_LEFT_ARROW })
+      end
+      num_cells = num_cells + 1
+    end
+  
+    while #cells > 0 do
+      local cell = table.remove(cells, 1)
+      push(cell, #cells == 0)
+    end
+  
+    window:set_right_status(wezterm.format(elements))
+  end)
 
 return config
